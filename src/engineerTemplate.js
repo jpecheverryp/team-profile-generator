c4dcc49c68cd71b18712c0036494e16ded133e8b
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-02 23:42:42 -0500

    created function to generate engineer cards using info

---
+++ src/engineerTemplate.js
@@ -0,0 +1,16 @@
+const engineerTemplate =
+`<div class="card>
+    <div class="card-header bg-primary text-white">
+        <h5 class="card-title">{{name}}</h5>
+        <h6>Engineer</h6>
+    </div>
+    <ul class="list-group">
+        <div class="card-body">
+            <li class="list-group-item">Id: <span>{{id}}</span></li>
+            <li class="list-group-item">Email: <a href="mailto:{{email}}">{{email}}</a></li>
+            <li class="list-group-item">Github: <a href="https://github.com/{{github}}" target="_blank">{{github}}</a></li>
+        </div>
+    </ul>
+</div>`;
+
+module.exports = engineerTemplate
