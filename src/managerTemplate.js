4a9f229a0f6123d8fd18524ce09220aa0fe03f74
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-02 23:28:34 -0500

    changed manager template, now a string in a js module, created function that replaces placeholders for info

---
+++ src/managerTemplate.js
@@ -0,0 +1,16 @@
+const managerCard = 
+`<div class="card">
+    <div class="card-header bg-primary text-white">
+        <h5 class="card-title">{{managerName}}</h5>
+        <h6>Manager</h6>
+    </div>
+    <ul class="list-group">
+        <div class="card-body">
+            <li class="list-group-item">Id: <span>{{id}}</span></li>
+            <li class="list-group-item">Email: <a href="mailto:{{managerEmail}}">{{managerEmail}}</a></li>
+            <li class="list-group-item">Office: <span>{{officeNumber}}</span></li>
+        </div>
+    </ul>
+</div>`;
+
+module.exports = managerCard;
