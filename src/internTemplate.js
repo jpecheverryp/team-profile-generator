6b9725b194b6bb621156de704d45a9b5177bfc65
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-02 23:37:10 -0500

    created function to generate cards for interns using template

--- src/internTemplate.js
+++ src/internTemplate.js
@@ -1,16 +1,16 @@
-const managerCard = 
-`<div class="card">
+const internTemplate =
+`<div class="card" >
     <div class="card-header bg-primary text-white">
-        <h5 class="card-title">{{managerName}}</h5>
-        <h6>Manager</h6>
+        <h5 class="card-title">{{name}}</h5>
+        <h6>Intern</h6>
     </div>
     <ul class="list-group">
         <div class="card-body">
             <li class="list-group-item">Id: <span>{{id}}</span></li>
             <li class="list-group-item">Email: <a href="mailto:{{email}}">{{email}}</a></li>
-            <li class="list-group-item">Office: <span>{{officeNumber}}</span></li>
+            <li class="list-group-item">School: <span>{{school}}</span></li>
         </div>
     </ul>
-</div>`;
+</div>`
 
-module.exports = managerCard;
+module.exports = internTemplate;
